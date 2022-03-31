b3491a3f6e4d46c0d08e6cf6e0adbd40bac3e3ad
jose <jose@arejo.com>
2022-03-31 17:30:04 -0500

    Funciona SMTP y POP3

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,40 +2,43 @@ const { Router } = require('express');
 const router = Router();
 
 const nodemailer = require('nodemailer');
+const pop3 = require('node-pop3')
 
 router.post('/send-email', async (req, res) => {
-    const { name, email, phone, message } = req.body;
+    const { from, to, subject, email, password, message } = req.body;
+    const pop3_client = new pop3 ({
+        user: email,
+        password: password,
+        host: 'arejo.com',
+    });
 
     contentHTML = `
-        <h1>User Information</h1>
-        <ul>
-            <li>Username: ${name}</li>
-            <li>User Email: ${email}</li>
-            <li>PhoneNumber: ${phone}</li>
-        </ul>
         <p>${message}</p>
     `;
 
     let transporter = nodemailer.createTransport({
-        host: 'mail.fazttech.net',
-        port: 587,
+        host: 'arejo.com',
+        port: 25,
         secure: false,
-        auth: {
-            user: 'testtwo@fazttech.net',
-            pass: 'testtwocontraseña'
-        },
+        // auth: {
+        //     user: 'arielito@arejo.com',
+        //     pass: 'arielito'
+        // },
         tls: {
             rejectUnauthorized: false
         }
     });
 
     let info = await transporter.sendMail({
-        from: '"FaztTech Server" <testtwo@fazttech.xyz>', // sender address,
-        to: 'fazttech@gmail.com',
-        subject: 'Website Contact Form',
+        from: email, // sender address,
+        to: to,
+        subject: subject,
         // text: 'Hello World'
         html: contentHTML
-    })
+    });
+
+    let retrieve = await pop3_client.RETR(1);
+    
 
     console.log('Message sent: %s', info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
@@ -43,6 +46,8 @@ router.post('/send-email', async (req, res) => {
     // Preview only available when sending through an Ethereal account
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    console.log(retrieve);
 
     res.redirect('/success.html');
 });
