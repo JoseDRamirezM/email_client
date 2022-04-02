9210fe32b75e25fd3e7c8e6bb9cc0866b49edff7
arefome <55114188+arefome@users.noreply.github.com>
2022-04-01 21:10:53 -0500

    add views

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-app.use(require('./routes/index'));
+app.use(require("./routes"));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,54 +2,77 @@ const { Router } = require('express');
 const router = Router();
 
 const nodemailer = require('nodemailer');
-const pop3 = require('node-pop3')
+const pop3 = require('node-pop3');
+let email, pwd, pop3_client;
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Correo Electrónico' });
+});
+
+router.get('/email', (req, res) => {
+    res.render('email', { title: 'Envía un correo' });
+});
+
+router.get('/inbox', async (req, res) => {
+    const list = await pop3_client.UIDL();
+    list.forEach( async element => {
+        let retrieve = await pop3_client.RETR(list[0]);
+        console.log(retrieve);
+    });
+    res.render('inbox', { title: 'Bandeja de entrada' });
+});
+
 
 router.post('/send-email', async (req, res) => {
-    const { from, to, subject, email, password, message } = req.body;
-    const pop3_client = new pop3 ({
-        user: email,
-        password: password,
-        host: 'arejo.com',
-    });
-
+    const { from, to, subject, message } = req.body;
     contentHTML = `
         <p>${message}</p>
     `;
+    console.log(req.body)
+    console.log(email, pwd)
+    // let transporter = nodemailer.createTransport({
+    //     host: 'arejo.com',
+    //     port: 25,
+    //     secure: false,
+    //     // auth: {
+    //     //     user: 'arielito@arejo.com',
+    //     //     pwd: 'arielito'
+    //     // },
+    //     tls: {
+    //         rejectUnauthorized: false
+    //     }
+    // });
 
-    let transporter = nodemailer.createTransport({
-        host: 'arejo.com',
-        port: 25,
-        secure: false,
-        // auth: {
-        //     user: 'arielito@arejo.com',
-        //     pass: 'arielito'
-        // },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    let info = await transporter.sendMail({
-        from: email, // sender address,
-        to: to,
-        subject: subject,
-        // text: 'Hello World'
-        html: contentHTML
-    });
-
-    let retrieve = await pop3_client.RETR(1);
+    // let info = await transporter.sendMail({
+    //     from: `${from} <${email}>`, // sender address,
+    //     to: to,
+    //     subject: subject,
+    //     // text: 'Hello World'
+    //     html: contentHTML
+    // });
     
 
-    console.log('Message sent: %s', info.messageId);
+    //console.log('Message sent: %s', info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    //console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-    console.log(retrieve);
+    //console.log(retrieve);
 
-    res.redirect('/success.html');
+    res.redirect('inbox');
+});
+
+router.post('/login', (req, res) => {
+    email, pwd = req.body;
+    pop3_client = new pop3 ({
+        user: email,
+        pwdword: pwd,
+        host: 'arejo.com',
+    });
+    console.log(email, pwd)
+    res.redirect('email');
 });
 
 module.exports = router;
